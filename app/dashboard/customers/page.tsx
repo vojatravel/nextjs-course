65ae4c5134592618893638672b04770b84b5a069
joppe2001 <joppe.montezinos@gmail.com>
2024-03-22 11:45:28 +0100

    Add new dashboard pages and layout components

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+const cutstomerPage = () => {
+  return (
+    <div>
+      <h1>Customers</h1>
+    </div>
+  );
+};
+
+export default cutstomerPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+const invoicesPage = () => {
+  return (
+    <div>
+      <h1>Invoices</h1>
+    </div>
+  );
+};
+
+export default invoicesPage;
